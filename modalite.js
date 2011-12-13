6d84ecb51e0cb592b549258553efc348f7cf7fa0
Jordan Adams <jordan_callum@live.co.uk>
2011-12-13 02:51:51 +0000

    Completed script, added themes, added examples

--- modalite.js
+++ modalite.js
@@ -8,10 +8,19 @@
 
 (function($) {
 	
-	$.modalite = function(content, width) {
+	$.modalite = function(content, options) {
+		
+		// Build Settings
+		defaults = {
+			theme: 'light',
+			width: 300
+		};
+		
+		settings = {};
+		settings = $.extend(defaults, options);
 
-		width ? true : width = 300;
-
+		
+		// Create DOM Elems
 		$('body').append(
 			'<div class="modalite_overlay"></div>'
 			+'<div class="modalite_modal">'
@@ -19,16 +28,44 @@
 				+'<div class="modalite_modal_close"><a href="#">&#10005; Close</div>'
 			+'</div>');
 
+
+		// Add Theme
+		switch (settings.theme) {
+			case 'light':
+				$('.modalite_modal').addClass('theme_light');
+				$('.modalite_modal_close').addClass('theme_light');
+				$('.modalite_overlay').addClass('theme_light');
+			break;
+			
+			case 'dark':
+				$('.modalite_modal').addClass('theme_dark');
+				$('.modalite_modal_close').addClass('theme_dark');
+				$('.modalite_overlay').addClass('theme_dark');
+			break;
+			
+			case 'facebook':
+				$('.modalite_modal').addClass('theme_facebook');
+				$('.modalite_modal_close').addClass('theme_facebook');
+				$('.modalite_overlay').addClass('theme_facebook');
+
+				$('.modalite_modal_close a').text('Close');
+				$('.modalite_modal').children().wrapAll('<div class="modalite_modal_facebook" />');
+
+			break;
+		}
+
+
+		// Position & Size
+		$('.modalite_overlay').css({
+			'width' : '100%',
+			'height' : '100%'
+		});
+		
+		$('.modalite_modal').css('width', settings.width + "px");
+
 		var wnd_w = window.innerWidth;
 		var wnd_h = window.innerHeight;
-
-		$('.modalite_modal').css('width', width + "px");
-
-		$('.modalite_overlay').css({
-			'width' : wnd_w,
-			'height' : wnd_h
-		});
-
+		
 		var mdlt_w = $('.modalite_modal').width();
 		var mdlt_h = $('.modalite_modal').height();
 
@@ -37,9 +74,25 @@
 
 		$('.modalite_modal').css('top', mdlt_t);
 		$('.modalite_modal').css('left', mdlt_l);
-
-		$('.modalite_overlay').fadeTo(350, 0.5);
-		$('.modalite_modal').fadeTo(350, 1);
+		
+		
+		// Display Modalite
+		switch (settings.theme) {
+			case 'light':
+				$('.modalite_overlay').fadeTo(350, 0.5);
+				$('.modalite_modal').fadeTo(350, 1);
+			break;
+			
+			case 'dark':
+				$('.modalite_overlay').fadeTo(350, 0.75);
+				$('.modalite_modal').fadeTo(350, 1);
+			break;
+			
+			case 'facebook':
+				$('.modalite_overlay').fadeTo(350, 0);
+				$('.modalite_modal').fadeTo(350, 1);
+			break;
+		}
 			
 	}
 
@@ -73,7 +126,7 @@
 
 	});
 	
-	$('.modalite_modal_close').live('click', function() {
+	$('.modalite_modal_close a').live('click', function() {
 	
 		modalite_destroy();
 	
